a6a30526faea925260cba05c9677368d5515c2ef
jdiaz483 <joselyndiaz483@gmail.com>
2022-07-04 17:08:39 -0400

    Completed Responsiveness

--- js/gallery02.js
+++ js/gallery02.js
@@ -1,3 +1,22 @@
+// Navbar Stuff 
+
+const navbarToggle = navbar.querySelector('#navbar-toggle');
+let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
+
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute('aria-expanded', isNavbarExpanded);
+};
+
+navbarToggle.addEventListener('click', toggleNavbarVisibility);
+
+const navbarMenu = document.querySelector('#navbar-menu');
+const navbarLinksContainer = navbarMenu.querySelector('.navbar-links');
+
+navbarLinksContainer.addEventListener('click', (e) => e.stopPropagation());
+navbarMenu.addEventListener('click', toggleNavbarVisibility);
+
+// Main Stuff 
 const searchBar = document.getElementById("searchBar");
 const searchButton = document.getElementById("searchSubmit"); 
 const gallerySection = document.getElementById("gallerySection"); 
